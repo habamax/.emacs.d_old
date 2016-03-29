25079d26ecbec30d9a41c8c292c4c10b9446aab9
Maxim Kim <habamax@gmail.com>
2016-03-29 23:35:49 +0300

    kosmos-theme tinkering.
    Changes in highlight, git-commit-summary, erc and header-line.

--- themes/kosmos-theme.el
+++ themes/kosmos-theme.el
@@ -2,26 +2,31 @@
 
 ;;; Commentary:
 ;; TODO: fix colors
-;; erc modeline activity status is invisible
+;; rcirc
+;; package-list-packages
+;; swiper
+;; incremental search
+;; TODO: change color names to color codes
+;; TODO: find color for git-commit-nonempty-second-line
+
 
 
 ;;; Code:
 
 
-(deftheme kosmos "Have you set up filters for you space suit?")
+(deftheme kosmos "Have you set up filters for you space suit? I have...")
 
 (custom-theme-set-faces
  'kosmos
 
- '(default ((t (:background "black" :foreground "gray80"))))
+ '(default ((t (:background "black" :foreground "#aaaaaa"))))
  '(cursor ((t (:background "Coral3"))))
  '(region ((t (:background "cadetblue4" :foreground "black"))))
- '(highlight ((t (:background "LightSteelBlue4" :foreground "White"))))
- 
+ '(highlight ((t (:background "#c1cdc1" :foreground "#000000"))))
  
  '(font-lock-string-face ((t (:foreground "wheat3"))))
  '(font-lock-comment-face ((t (:foreground "cyan3"))))
- '(font-lock-keyword-face ((t (:foreground "white" :slant italic))))
+ '(font-lock-keyword-face ((t (:foreground "#dddddd" :slant italic))))
  
  '(font-lock-builtin-face ((t nil)))
  '(font-lock-type-face ((t nil)))
@@ -49,20 +54,28 @@
  '(company-preview ((t (:inherit company-tooltip-selection))))
  '(company-preview-common ((t (:inherit company-preview :slant italic))))
 
- '(erc-nick-default-face ((t (:foreground "Gray60"))))
- '(erc-current-nick-face ((t (:foreground "Gray60" :slant italic))))
-
+ 
+ '(erc-current-nick-face ((t (:foreground "#ffffff"))))
+ '(erc-nick-default-face ((t (:foreground "#779977"))))
+ '(erc-my-nick-face ((t (:slant italic))))
+ '(erc-input-face ((t (:foreground "#cccccc"))))
  '(erc-timestamp-face ((t (:foreground "Wheat"))))
- '(erc-notice-face ((t (:foreground "gray40"))))
+ '(erc-notice-face ((t (:foreground "#555555"))))
  '(erc-action-face ((nil (:slant italic))))
  '(erc-button ((t (:underlined on))))
 
+ '(header-line ((t (:foreground "#ffffff" :background "#333333" :box (:line-width 1 :color "#555555" :style released-button)))))
+
+ '(git-commit-summary ((t (:inherit font-lock-string-face))))
+
+
  '(org-level-1 ((t (:height 1.5))))
- '(org-level-2 ((t (:height 1.4))))
- '(org-level-3 ((t (:height 1.3))))
- '(org-level-4 ((t (:slant italic :height 1.2))))
- '(org-level-5 ((t (:slant italic :height 1.2))))
- '(org-level-6 ((t (:slant italic :height 1.2))))
+ '(org-level-2 ((t (:height 1.25))))
+ '(org-level-3 ((t (:height 1.125))))
+ 
+ '(org-level-4 ((t (:slant italic :height 1.1))))
+ '(org-level-5 ((t (:slant italic :height 1.1))))
+ '(org-level-6 ((t (:slant italic :height 1.1))))
 
 
  )
