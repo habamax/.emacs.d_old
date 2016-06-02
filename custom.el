8be8f8d7b78b0bfeded41f5d7e8c9bbd13c5f2dc
Kim <maksim.kim@adastracorp.net>
2016-06-02 16:14:51 +0300

    agenda files

--- custom.el
+++ custom.el
@@ -3,9 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files
-   (quote
-    ("~/org/refile.org" "~/org/todo.org" "~/org/adastra/adastra-setup.org"))))
+ '(org-agenda-files (quote ("~/org/refile.org" "~/org/todo.org"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
