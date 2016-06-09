9820aafa0ce30dbc919407c8f2b90b51fd805fd1
Maxim Kim <habamax@gmail.com>
2016-06-09 18:22:17 +0300

    org latex classes "parametrization"

--- lisp/haba-latex.el
+++ lisp/haba-latex.el
@@ -44,18 +44,33 @@
          \\usepackage{polyglossia}
          \\setdefaultlanguage{" lang-main "}
          \\setotherlanguages{" lang-other "}"))
-(defun haba/org-latex-class-hf-logo-2(logo-left logo-right)
+
+(defun haba/org-latex-class-hf-logo-2(logo-left vmove-left logo-right vmove-right)
   (interactive "P")
   (format "\\usepackage{fancyhdr}
              \\pagestyle{fancy}
              \\fancypagestyle{plain}{\\pagestyle{fancy}}
-             \\lhead{\\includegraphics[scale=1]{%s/logo/%s}}
+             %s
              \\chead{}
-             \\rhead{\\includegraphics[scale=1,trim=0 -3mm 0 0]{%s/logo/%s}}
+             %s
              \\lfoot{} \\cfoot{} \\rfoot{\\thepage}
              \\renewcommand{\\headrulewidth}{0.0pt}
              \\renewcommand{\\footrulewidth}{0.0pt}"
-          org-directory logo-left org-directory logo-right))
+          (if (string= logo-left "")
+              "\\lhead{}"
+            (format
+             "\\lhead{\\includegraphics[scale=1%s]{%s/logo/%s}}"
+             (if (string= vmove-left "") "" (format ",trim=0 %s 0 0" vmove-left))
+             org-directory logo-left))
+          (if (string= logo-right "")
+              "\\rhead{}"
+            (format
+             "\\rhead{\\includegraphics[scale=1%s]{%s/logo/%s}}"
+             (if (string= vmove-right "") "" (format ",trim=0 %s 0 0" vmove-right))
+             org-directory logo-right))))
+
+
+
 (setq haba/org-latex-class-titling
       "\\usepackage{titling}
          \\pretitle{\\begin{center}\\LARGE\\bfseries\\sffamily}
@@ -89,13 +104,14 @@
          \\usepackage{titlesec}
          \\titleformat{\\chapter}{\\normalfont\\LARGE\\bfseries\\sffamily}{\\thechapter.}{1em}{}
          \\titlespacing*{\\chapter}{0pt}{3.5ex plus 1ex minus .2ex}{2.3ex plus .2ex} ")
-(defun haba/org-latex-class-titling-logo-2 (logo-left logo-right)
+
+(defun haba/org-latex-class-titling-logo-2 (logo-left vmove-left logo-right vmove-right)
   (format "\\usepackage{titling}
              \\pretitle{
              \\vspace{-35mm}\\hspace{-5mm}
-             \\includegraphics[scale=1]{%s/logo/%s}
-             \\\hspace{\\stretch{1}}
-             \\includegraphics[scale=1,trim=0 -3mm 0 0]{%s/logo/%s}
+             %s
+             \\hspace{\\stretch{1}}
+             %s
              \\vspace{35mm}\\vspace{\\stretch{1}}
              \\begin{center}\\LARGE\\bfseries\\sffamily}
              \\posttitle{\\par\\end{center}\\vspace{\\stretch{1.5}}}
@@ -103,8 +119,19 @@
              \\postauthor{\\par\\end{center}}
              \\predate{\\begin{center}\\normalsize\\sffamily}
              \\date{}
-             \\postdate{\\par\\end{center}}" 
-          org-directory logo-left org-directory logo-right))
+             \\postdate{\\par\\end{center}}"
+          (if (string= logo-left "")
+              ""
+            (format
+             "\\includegraphics[scale=1%s]{%s/logo/%s}"
+             (if (string= vmove-left "") "" (format ",trim=0 %s 0 0" vmove-left))
+             org-directory logo-left))
+          (if (string= logo-right "")
+              ""
+            (format
+             "\\includegraphics[scale=1%s]{%s/logo/%s}"
+             (if (string= vmove-right "") "" (format ",trim=0 %s 0 0" vmove-right))
+             org-directory logo-right))))
 
 (add-to-list 'org-latex-classes
              `("article"
@@ -142,7 +169,7 @@
                ,(concat "\\documentclass[a4paper,12pt]{article}"
                         (haba/org-latex-class-common "russian" "english")
                         "\\usepackage[top=30mm, left=30mm, right=20mm, bottom=35mm]{geometry}"
-                        (haba/org-latex-class-hf-logo-2 "logo_sberbank_simple_50_opacity.png" "logo_adastra_50_opacity.png")
+                        (haba/org-latex-class-hf-logo-2 "logo_sberbank_simple_50_opacity.png" "" "logo_adastra_50_opacity.png" "-3mm")
                         haba/org-latex-class-titling
                         haba/org-latex-class-dot-in-chapters
                         haba/org-latex-class-fancy-quoteblock)
@@ -177,8 +204,26 @@
                ,(concat "\\documentclass[a4paper,12pt]{report}"
                         (haba/org-latex-class-common "russian" "english")
                         "\\usepackage[top=30mm, left=30mm, right=25mm, bottom=35mm]{geometry}"
-                        (haba/org-latex-class-hf-logo-2 "logo_sberbank_simple_50_opacity.png" "logo_adastra_50_opacity.png")
-                        (haba/org-latex-class-titling-logo-2 "logo_sberbank_simple_50_opacity.png" "logo_adastra_50_opacity.png")
+                        (haba/org-latex-class-hf-logo-2 "logo_sberbank_simple_50_opacity.png" "" "logo_adastra_50_opacity.png" "-3mm")
+                        (haba/org-latex-class-titling-logo-2 "logo_sberbank_simple_50_opacity.png" "" "logo_adastra_50_opacity.png" "-3mm")
+                        haba/org-latex-class-dot-in-chapters
+                        haba/org-latex-class-fancy-quoteblock
+                        haba/org-latex-class-no-chapters)
+
+               ("\\chapter{%s}" . "\\chapter*{%s}")
+               ("\\section{%s}" . "\\section*{%s}")
+               ("\\subsection{%s}" . "\\subsection*{%s}")
+               ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+               ("\\paragraph{%s}" . "\\paragraph*{%s}")
+               ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
+
+(add-to-list 'org-latex-classes
+             `("report-adastra-ru"
+               ,(concat "\\documentclass[a4paper,12pt]{report}"
+                        (haba/org-latex-class-common "russian" "english")
+                        "\\usepackage[top=30mm, left=30mm, right=25mm, bottom=35mm]{geometry}"
+                        (haba/org-latex-class-hf-logo-2 "logo_adastra_50_opacity.png" "" "" "")
+                        (haba/org-latex-class-titling-logo-2 "logo_adastra_50_opacity.png" "" "" "")
                         haba/org-latex-class-dot-in-chapters
                         haba/org-latex-class-fancy-quoteblock
                         haba/org-latex-class-no-chapters)
