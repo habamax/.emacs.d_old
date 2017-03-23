05d60f9d517ab88d69af89ceddbd1f173a0a7aef
Maxim Kim <habamax@gmail.com>
2017-03-23 18:24:16 +0300

    Dumb jump FTW

--- init.el
+++ init.el
@@ -204,6 +204,14 @@
   :bind (("C-;" . avy-goto-word-1)
          ("M-g g" . avy-goto-line)))
 
+
+(use-package dumb-jump
+  :bind (("M-g o" . dumb-jump-go-other-window)
+         ("M-g j" . dumb-jump-go)
+         ("M-g x" . dumb-jump-go-prefer-external)
+         ("M-g z" . dumb-jump-go-prefer-external-other-window))
+  :config (setq dumb-jump-selector 'ivy))
+
 ;; (use-package ido
 ;;   :config
 ;;   (setq ido-enable-flex-matching t)
