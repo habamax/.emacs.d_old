8acf5d7cbe39224d68e9e06b39f8a4a9dfed15df
Maxim Kim <habamax@gmail.com>
2017-04-14 15:55:44 +0300

    * init.el (tab-always-indent): when ivy complete works -- it is super useful

--- init.el
+++ init.el
@@ -59,6 +59,7 @@
 ;; winner mode is a must
 (winner-mode 1)
 
+(setq tab-always-indent 'complete)
 
 ;; tabs are evil...
 (setq-default indent-tabs-mode nil)
@@ -255,11 +256,12 @@
   :bind (("M-x" . counsel-M-x)
          ("C-x C-f" . counsel-find-file)
          ("M-s s" . counsel-grep-or-swiper)
+         ("M-s r" . counsel-rg)
          ("C-c s" . swiper-all)
          ("C-x b" . ivy-switch-buffer)
          ("M-y" . counsel-yank-pop))
   :config
-  ;; counsel uses smex for better soring
+  ;; counsel uses smex for better sorting
   (use-package smex :defer)
 
   (setq counsel-find-file-at-point t)
@@ -272,6 +274,8 @@
 
   (setq ivy-ignore-buffers '(".*-autoloads.el"))
   (setq ivy-switch-buffer-faces-alist '((dired-mode . ivy-subdir)))
+
+  (setq counsel-yank-pop-separator (concat "\n" (make-string 70 ?-) "\n"))
   )
 
 
@@ -485,6 +489,13 @@ _P_: Ivy pop view         _-_: - height        _m_: Maximize current
   (use-package cider
     :config
     (setq cider-repl-display-help-banner nil)))
+
+(use-package slime
+  :commands (slime)
+  :config
+  (setq inferior-lisp-program "ros -Q run")
+  (slime-setup '(slime-fancy))
+  )
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Built-in packages
