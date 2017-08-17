bc4446fc63b165bb903b9ded65c36cfcaf6f39d3
Maxim Kim <habamax@gmail.com>
2017-08-17 11:22:43 +0300

    Function to pretty-print XML region

--- lisp/haba-stuff.el
+++ lisp/haba-stuff.el
@@ -253,3 +253,20 @@ line instead."
       (delete-region (car bounds) (cdr bounds))))
   (insert (format-time-string "%Y-%m-%d"))
   )
+
+;;;; Pretty print XML
+(defun haba/pretty-print-xml-region (begin end)
+  "Pretty format XML markup in region. You need to have nxml-mode
+http://www.emacswiki.org/cgi-bin/wiki/NxmlMode installed to do
+this.  The function inserts linebreaks to separate tags that have
+nothing but whitespace between them.  It then indents the markup
+by using nxml's indentation rules."
+  (interactive "r")
+  (save-excursion
+    (nxml-mode)
+    (goto-char begin)
+    (while (search-forward-regexp "\>[ \\t]*\<" nil t)
+      (backward-char) (insert "\n"))
+    (indent-region begin end)
+    (normal-mode))
+  (message "Prettified! Did my best!"))
