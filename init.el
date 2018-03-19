2a54cc05a12a6be7d78ca018659348c9f649af04
Maxim Kim <habamax@gmail.com>
2018-03-19 20:30:36 +0300

    dired open in the same window with mouseclick

--- init.el
+++ init.el
@@ -888,6 +888,27 @@
     (let ((file (ignore-errors (dired-get-file-for-visit))))
       (browse-url (file-truename file))))
 
+  (defun haba/dired-mouse-find-file (event)
+    "In Dired, visit the file or directory name you click on in the same window."
+    (interactive "e")
+    (let (window pos file)
+      (save-excursion
+        (setq window (posn-window (event-end event))
+              pos (posn-point (event-end event)))
+        (if (not (windowp window))
+            (error "No file chosen"))
+        (set-buffer (window-buffer window))
+        (goto-char pos)
+        (setq file (dired-get-file-for-visit)))
+      (if (file-directory-p file)
+          (or (and (cdr dired-subdir-alist)
+                   (dired-goto-subdir file))
+              (progn
+                (select-window window)
+                (dired-find-file)))
+        (select-window window)
+        (find-file (file-name-sans-versions file t)))))
+
   ;; need for -select function
   (require 'dash)
 
