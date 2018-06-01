a83c1f7ea3bdd8c34e0ddc52bca3ee427093868c
Maxim Kim <habamax@gmail.com>
2018-06-01 10:56:02 +0300

    fix indentation

--- lisp/haba-stuff.el
+++ lisp/haba-stuff.el
@@ -121,7 +121,7 @@ remove the comment characters from that line."
 
 
 (defun haba/move-beginning-of-line (arg)
-    "Move point back to indentation of beginning of line.
+  "Move point back to indentation of beginning of line.
 
 Move point to the first non-whitespace character on this line.
 If point is already there, move to the beginning of the line.
@@ -130,18 +130,18 @@ the beginning of the line.
 
 If ARG is not nil or 1, move forward ARG - 1 lines first. If
 point reaches the beginning or end of the buffer, stop there."
-    (interactive "^p")
-    (setq arg (or arg 1))
+  (interactive "^p")
+  (setq arg (or arg 1))
 
-    ;; Move lines first
-    (when (/= arg 1)
-      (let ((line-move-visual nil))
-        (forward-line (1- arg))))
+  ;; Move lines first
+  (when (/= arg 1)
+    (let ((line-move-visual nil))
+      (forward-line (1- arg))))
 
-    (let ((orig-point (point)))
-      (back-to-indentation)
-      (when (= orig-point (point))
-        (move-beginning-of-line 1))))
+  (let ((orig-point (point)))
+    (back-to-indentation)
+    (when (= orig-point (point))
+      (move-beginning-of-line 1))))
 
 
 
